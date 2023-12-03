5fba2fdaad882032a1ad2a370076364dcc9254e6
MuAdeel <117102045+MuAdeel@users.noreply.github.com>
2023-12-03 06:03:43 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = true
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
