50ac76b0136c8e6a9153809d9a9d4ccc9a6bd0dc
MuAdeel <117102045+MuAdeel@users.noreply.github.com>
2023-12-03 05:42:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 54
+let accountEmail = "adeel@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chakwal"
+let accountState;
+
+// accountId = 50
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+let name = "Muhammad Adeel"
+let age = 22
+let city = "Chakwal"
+let student = true
+let university = null
+
+console.table([name, age, city, student, university])
